5f46e5bf398243f7acd4ba4fbb014c897c5760ff
LeoZhang <njustzhangl@163.com>
2023-12-26 17:04:28 +0800

    [R4R]-[eth_getLogs]feat: fix eth_getLogs for legacy tx

--- core/rawdb/accessors_chain.go
+++ core/rawdb/accessors_chain.go
@@ -718,16 +718,37 @@ type receiptLogs struct {
 
 // DecodeRLP implements rlp.Decoder.
 func (r *receiptLogs) DecodeRLP(s *rlp.Stream) error {
-	var stored storedReceiptRLP
-	if err := s.Decode(&stored); err == nil {
+	blob, err := s.Raw()
+	if err != nil {
+		return err
+	}
+
+	// First try to decode the latest receipt database format, try the pre-bedrock Optimism legacy format otherwise.
+	stored := new(storedReceiptRLP)
+	if err := decodeStoredReceiptRLP(stored, blob); err == nil {
 		r.Logs = stored.Logs
 		return nil
 	}
-	var storedLegacy legacyOptimismStoredReceiptRLP
-	if err := s.Decode(&storedLegacy); err != nil {
+
+	storedLegacy := new(legacyOptimismStoredReceiptRLP)
+	if err := decodeLegacyOptimismReceiptRLP(storedLegacy, blob); err != nil {
 		return err
 	}
 	r.Logs = stored.Logs
+	return nil
+}
+
+func decodeStoredReceiptRLP(stored *storedReceiptRLP, blob []byte) error {
+	if err := rlp.DecodeBytes(blob, stored); err != nil {
+		return err
+	}
+	return nil
+}
+
+func decodeLegacyOptimismReceiptRLP(stored *legacyOptimismStoredReceiptRLP, blob []byte) error {
+	if err := rlp.DecodeBytes(blob, stored); err != nil {
+		return err
+	}
 	return nil
 }
 
