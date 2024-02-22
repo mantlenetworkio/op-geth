52122bd87d2b78c3622c94a8e50776b8547bdc81
Tri-stone <njustzhangl@163.com>
2024-02-22 17:49:38 +0800

    [R4R]-[tx]feat: fix tx cost (#41)

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -374,12 +374,13 @@ func (tx *Transaction) IsSystemTx() bool {
 	return tx.inner.isSystemTx()
 }
 
-// Cost returns gas * gasPrice + value.
+// Cost returns (gas * gasPrice) + (blobGas * blobGasPrice) + value.
 func (tx *Transaction) Cost() *big.Int {
 	total := new(big.Int).Mul(tx.GasPrice(), new(big.Int).SetUint64(tx.Gas()))
 	if tx.Type() == BlobTxType {
 		total.Add(total, new(big.Int).Mul(tx.BlobGasFeeCap(), new(big.Int).SetUint64(tx.BlobGas())))
 	}
+	total.Add(total, tx.Value())
 	return total
 }
 
