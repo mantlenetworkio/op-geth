0bd29f8ffd7c720e78bb12a889d786a0b1da2c5f
WangFeifan <wff19940326@gmail.com>
2024-03-06 18:12:01 +0800

    R4R: fix_N-05 Unused Variables (#54)

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -26,7 +26,7 @@ var (
 	// The base fee portion of the transaction fee accumulates at this predeploy
 	OptimismBaseFeeRecipient = common.HexToAddress("0x4200000000000000000000000000000000000019")
 	// The L1 portion of the transaction fee accumulates at this predeploy
-	OptimismL1FeeRecipient = common.HexToAddress("0x420000000000000000000000000000000000001A")
+	// OptimismL1FeeRecipient = common.HexToAddress("0x420000000000000000000000000000000000001A")
 )
 
 const (
