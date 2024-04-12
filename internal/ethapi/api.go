ff9982977645a3bddbe2b8498d2dd108b422df94
Ivan Wang <ivan.wang@mantle.xyz>
2024-04-12 16:40:32 +0800

    fix estimateGas eat evm error

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1326,7 +1326,7 @@ func DoEstimateGas(ctx context.Context, b Backend, args TransactionArgs, blockNr
 	// Execute the binary search and hone in on an executable gas limit
 	for lo+1 < hi {
 		mid := (hi + lo) / 2
-		failed, _, err := executable(mid)
+		failed, result, err := executable(mid)
 
 		// If the error is not nil(consensus error), it means the provided message
 		// call or transaction will never be accepted no matter how much gas it is
@@ -1334,6 +1334,14 @@ func DoEstimateGas(ctx context.Context, b Backend, args TransactionArgs, blockNr
 		if err != nil {
 			return 0, err
 		}
+
+		if result != nil && result.Err != vm.ErrOutOfGas {
+			if len(result.Revert()) > 0 {
+				return 0, newRevertError(result)
+			}
+			return 0, result.Err
+		}
+
 		if failed {
 			lo = mid
 		} else {
