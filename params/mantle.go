7455a8392af6145d82b322c750f7a3ea66af0a55
ivan <314130948@qq.com>
2025-08-25 18:43:59 +0800

    reth-debug

--- params/mantle.go
+++ params/mantle.go
@@ -51,7 +51,7 @@ var (
 		MetaTxV3UpgradeTime:   u64Ptr(0),
 		ProxyOwnerUpgradeTime: nil,
 		MantleEverestTime:     u64Ptr(0),
-		MantleSkadiTime:       u64Ptr(0),
+		MantleSkadiTime:       nil,
 	}
 	MantleDefaultUpgradeConfig = MantleUpgradeChainConfig{
 		BaseFeeTime:           u64Ptr(0),
