2197e615846d11d495e910b3d2317c08686b564a
pandainzoo <gary.zhu@mantle.xyz>
2025-11-21 14:49:12 +0800

    update qa3 info
    Signed-off-by: pandainzoo <gary.zhu@mantle.xyz>

--- params/mantle.go
+++ params/mantle.go
@@ -44,7 +44,7 @@ var (
 		ProxyOwnerUpgradeTime: nil,
 		MantleEverestTime:     u64Ptr(0),
 		MantleSkadiTime:       u64Ptr(0),
-		MantleLimbTime:        u64Ptr(1_763_719_200),
+		MantleLimbTime:        u64Ptr(1_763_794_800),
 	}
 	MantleLocalUpgradeConfig = MantleUpgradeChainConfig{
 		ChainID:               MantleLocalChainId,
