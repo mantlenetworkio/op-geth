3bb56c3a697bb0bce659cc1cf1facbccb2a88d87
sanbao <baobaobao369@163.com>
2026-04-07 10:27:22 +0800

    feat: add arsia time on mantle mainnet

--- params/mantle.go
+++ params/mantle.go
@@ -22,7 +22,7 @@ var (
 		MantleEverestTime:     u64Ptr(1_742_367_600),
 		MantleSkadiTime:       u64Ptr(1_756_278_000),
 		MantleLimbTime:        u64Ptr(1_768_374_000),
-		MantleArsiaTime:       nil,
+		MantleArsiaTime:       u64Ptr(1_776_841_200),
 	}
 	MantleSepoliaUpgradeConfig = MantleUpgradeChainConfig{
 		ChainID:               MantleSepoliaChainId,
